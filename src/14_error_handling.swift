4cc6f3652f6a174d75900eaa6290a95917a61255
Youngmin Gwon <youngmin-gwon@users.noreply.github.com>
2023-09-17 18:22:47 +0900

    ✨ Feat: Add rethrow and results for error handling

--- src/14_error_handling.swift
+++ src/14_error_handling.swift
@@ -81,3 +81,129 @@ do {
 } catch {
   print("Some other error")
 }
+
+// without do/catch statement
+if let yourCar = try? Car(manufacturer: "Tesla") {
+  print("Success, your car = \(yourCar)")
+} else {
+  print("Failed to construct and error is not accessible now")
+}
+
+struct Dog {
+  let isInjured: Bool
+  let isSleeping: Bool
+
+  enum BarkingErrors: Error {
+    case cannotBarkIsSleeping
+  }
+
+  enum RunningErrors: Error {
+    case cannotRunIsInjured
+  }
+
+  func bark() throws {
+    if isSleeping {
+      throw BarkingErrors.cannotBarkIsSleeping
+    }
+    print("Barking...")
+  }
+
+  func run() throws {
+    if isInjured {
+      throw RunningErrors.cannotRunIsInjured
+    }
+    print("Running...")
+  }
+
+  func barkAndRun() throws {
+    try bark()
+    try run()
+  }
+}
+
+let dog = Dog(isInjured: true, isSleeping: true)
+
+do {
+  try dog.barkAndRun()
+} catch Dog.BarkingErrors.cannotBarkIsSleeping, Dog.RunningErrors.cannotRunIsInjured {
+  print("Cannot-bark-is-sleeping OR cannot-run-is-injured")
+} catch {
+  print("Some other error")
+}
+
+do {
+  try dog.barkAndRun()
+} catch Dog.BarkingErrors.cannotBarkIsSleeping {
+  print("Cannot-bark-is-sleeping")
+} catch Dog.RunningErrors.cannotRunIsInjured {
+  print("cannot-run-is-injured")
+} catch {
+  print("Some other error")
+}
+
+// rethrows
+func fullName(
+  firstName: String?,
+  lastName: String?,
+  calculator: (String?, String?) throws -> String?
+) rethrows -> String? {
+  return try calculator(firstName, lastName)
+}
+
+enum NameErrors: Error {
+  case firstNameInvalid
+  case lastNameInvalid
+}
+
+func + (firstName: String?, lastName: String?) throws -> String? {
+  guard let firstName, !firstName.isEmpty else {
+    throw NameErrors.firstNameInvalid
+  }
+
+  guard let lastName, !lastName.isEmpty else {
+    throw NameErrors.lastNameInvalid
+  }
+
+  return "\(firstName) \(lastName)"
+}
+
+do {
+  let fooBar = try fullName(firstName: nil, lastName: nil, calculator: +)
+  print(fooBar ?? "")
+} catch NameErrors.firstNameInvalid {
+  print("First name is invalid")
+} catch NameErrors.lastNameInvalid {
+  print("Last name is invalid")
+} catch let err {
+  print("Some other error = \(err)")
+}
+
+// `results`
+// - either in other languages
+// - useful when writing API
+
+enum IntegerErrors: Error {
+  case noPositiveIntegerBefore(thisValue: Int)
+}
+
+func getPreviousPositiveInteger(from int: Int) -> Result<Int, IntegerErrors> {
+  guard int > 0 else {
+    return Result.failure(IntegerErrors.noPositiveIntegerBefore(thisValue: int))
+  }
+  return Result.success(int - 1)
+}
+
+func performGet(forValue value: Int) {
+  switch getPreviousPositiveInteger(from: value) {
+  case let .success(previousValue):
+    print("Previous Value is \(previousValue)")
+  case let .failure(error):
+    switch error {
+    case let .noPositiveIntegerBefore(thisValue):
+      print("No positive integer before \(thisValue)")
+    }
+  }
+}
+
+performGet(forValue: 0)
+performGet(forValue: 2)
